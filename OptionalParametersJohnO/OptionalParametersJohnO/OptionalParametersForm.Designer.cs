60200661c327a97dafcc0dddbfe56d57f1bc7e9f
John Omage <47784108+s422862@users.noreply.github.com>
2020-06-08 11:17:30 -0400

    Add files via upload

---
+++ OptionalParametersJohnO/OptionalParametersJohnO/OptionalParametersForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace OptionalParametersJohnO
+{
+    partial class frmOptionalParameters
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApartmentNum = new System.Windows.Forms.Label();
+            this.txtAptNum = new System.Windows.Forms.TextBox();
+            this.lblStreetAddress = new System.Windows.Forms.Label();
+            this.txtStreetAddress = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblApartmentNum
+            // 
+            this.lblApartmentNum.AutoSize = true;
+            this.lblApartmentNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApartmentNum.Location = new System.Drawing.Point(12, 51);
+            this.lblApartmentNum.Name = "lblApartmentNum";
+            this.lblApartmentNum.Size = new System.Drawing.Size(200, 24);
+            this.lblApartmentNum.TabIndex = 1;
+            this.lblApartmentNum.Text = "Apt Number (optional):";
+            // 
+            // txtAptNum
+            // 
+            this.txtAptNum.Location = new System.Drawing.Point(236, 56);
+            this.txtAptNum.Name = "txtAptNum";
+            this.txtAptNum.Size = new System.Drawing.Size(100, 20);
+            this.txtAptNum.TabIndex = 6;
+            // 
+            // lblStreetAddress
+            // 
+            this.lblStreetAddress.AutoSize = true;
+            this.lblStreetAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStreetAddress.Location = new System.Drawing.Point(12, 106);
+            this.lblStreetAddress.Name = "lblStreetAddress";
+            this.lblStreetAddress.Size = new System.Drawing.Size(138, 24);
+            this.lblStreetAddress.TabIndex = 7;
+            this.lblStreetAddress.Text = "Street Address:";
+            // 
+            // txtStreetAddress
+            // 
+            this.txtStreetAddress.Location = new System.Drawing.Point(236, 111);
+            this.txtStreetAddress.Name = "txtStreetAddress";
+            this.txtStreetAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtStreetAddress.TabIndex = 8;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(12, 171);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(45, 24);
+            this.lblCity.TabIndex = 9;
+            this.lblCity.Text = "City:";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(236, 176);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 10;
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProvince.Location = new System.Drawing.Point(12, 239);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(89, 24);
+            this.lblProvince.TabIndex = 11;
+            this.lblProvince.Text = "Province:";
+            // 
+            // txtProvince
+            // 
+            this.txtProvince.Location = new System.Drawing.Point(236, 244);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(100, 20);
+            this.txtProvince.TabIndex = 12;
+            // 
+            // lblPostalCode
+            // 
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPostalCode.Location = new System.Drawing.Point(12, 302);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(116, 24);
+            this.lblPostalCode.TabIndex = 13;
+            this.lblPostalCode.Text = "Postal Code:";
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(236, 307);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(100, 20);
+            this.txtPostalCode.TabIndex = 14;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(116, 369);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(149, 44);
+            this.btnEnter.TabIndex = 15;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // frmOptionalParameters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 450);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.txtStreetAddress);
+            this.Controls.Add(this.lblStreetAddress);
+            this.Controls.Add(this.txtAptNum);
+            this.Controls.Add(this.lblApartmentNum);
+            this.Name = "frmOptionalParameters";
+            this.Text = "Optional Parameters";
+            this.Load += new System.EventHandler(this.frmOptionalParameters_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApartmentNum;
+        private System.Windows.Forms.TextBox txtAptNum;
+        private System.Windows.Forms.Label lblStreetAddress;
+        private System.Windows.Forms.TextBox txtStreetAddress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label lblProvince;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ OptionalParametersJohnO/OptionalParametersJohnO/OptionalParametersForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptionalParametersJohnO
+{
+    public partial class frmOptionalParameters : Form
+    {
+        public frmOptionalParameters()
+        {
+            InitializeComponent();
+        }
+        // Procedure: PrintAddress
+        // Input: String aptNumber, String streetAddress, String city, String province, String postalCode
+        // Output: void
+        // Description: This procedure displays a message box with the given five parameters: 
+        // aptNumber, streetAddress, city, province and postalCode
+        public void PrintAddress(String aptNumber, String streetAddress, String city, String province, String postalCode)
+        {
+            // Display the address
+            MessageBox.Show("Your address is:" + aptNumber + " " + streetAddress + " " + city + " " + province + " " + postalCode);
+        }
+
+        // Procedure: PrintAddress
+        // Input: String streetAddress, String city, String province, String postalCode
+        // Output: void
+        // Description: This procedure displays a message box with the given four parameters: 
+        // streetAddress, city, province and postalCode
+        public void PrintAddress(String streetAddress, String city, String province, String postalCode)
+        {
+            // Display the address
+            MessageBox.Show("Your address is:" + " " + streetAddress + " " + city + " " + province + " " + postalCode);
+        }
+        private void frmOptionalParameters_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            // declare variables
+            String userAptNumber, userStreetAddress, userCity, userProvince, userPostalCode;
+
+            // get the info from text boxes
+            userAptNumber = this.txtAptNum.Text;
+            userStreetAddress = this.txtStreetAddress.Text;
+            userCity = this.txtCity.Text;
+            userProvince = this.txtProvince.Text;
+            userPostalCode = this.txtPostalCode.Text;
+
+            // make sure if user enters streetAddress
+            if (userStreetAddress == "")
+            {
+                // show a message box to enter street address
+                MessageBox.Show("Please enter your street address.");
+            }
+            // make sure user enters a city
+            else if (userCity == "")
+            {
+                // show a message to enter province
+                MessageBox.Show("Please enter your province");
+            }
+            // make sure user enters a postal code
+            else if (userPostalCode == "")
+            {
+                // show a message to enter postal code
+                MessageBox.Show("Please enter your postal code");
+            }
+            // if the user does not enter aptNumber, call the procedure that accepts only 4 string parameters
+            else if (userAptNumber == "")
+            {
+                PrintAddress(userStreetAddress, userCity, userProvince, userPostalCode);
+            }
+            // if the user enters all of the information, call the procedure that accepts 5 string parameters
+            else
+            {
+                PrintAddress(userAptNumber, userStreetAddress, userCity, userProvince, userPostalCode);
+            }
+        }
+    }
+}
