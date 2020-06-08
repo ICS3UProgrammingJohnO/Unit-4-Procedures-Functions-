860eeefe6debeef6f8bfc13299f040d00f81f4f2
John Omage <47784108+s422862@users.noreply.github.com>
2020-06-08 11:29:59 -0400

    Add files via upload

---
+++ FunctionsJohnO/FunctionsJohnO/FunctionsForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace FunctionsJohnO
+{
+    partial class frmFunctions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(47, 76);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(71, 13);
+            this.lblRadius.TabIndex = 1;
+            this.lblRadius.Text = "Enter Radius:";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(217, 76);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 3;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(49, 159);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(69, 13);
+            this.lblHeight.TabIndex = 4;
+            this.lblHeight.Text = "Enter Height:";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(217, 156);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(133, 225);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(118, 55);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate the volume of a cylinder";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(49, 309);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(35, 13);
+            this.lblVolume.TabIndex = 7;
+            this.lblVolume.Text = "label3";
+            // 
+            // frmFunctions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 376);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblRadius);
+            this.Name = "frmFunctions";
+            this.Text = "Functions by John O";
+            this.Load += new System.EventHandler(this.frmFunctions_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblVolume;
+    }
+}
+
